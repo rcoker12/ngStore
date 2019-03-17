d80b4e19a4f0bbd1cf4c0afa4d593530b0579aab
Bob Coker <rcoker06@gmail.com>
2019-03-17 14:56:26 -0500

    Start adding bootstrap and view changes

--- ngStore/Startup.cs
+++ ngStore/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 
 namespace ngStore
 {
@@ -15,14 +16,28 @@ namespace ngStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseDefaultFiles();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/error");
+            }
+
             app.UseStaticFiles();
             app.UseNodeModules(env);
+
+            app.UseMvc(cfg =>
+            {
+                cfg.MapRoute("Default", "/{controller}/{action}/{id?}", new { controller = "Home", Action = "Index" });
+            });
         }
     }
 }
