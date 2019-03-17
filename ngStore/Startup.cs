8b5c38ee0c989dea551d2c336c53caa348952e22
Bob Coker <rcoker06@gmail.com>
2019-03-17 12:12:56 -0500

    Update before controllers and views

--- ngStore/Startup.cs
+++ ngStore/Startup.cs
@@ -20,15 +20,9 @@ namespace ngStore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseNodeModules(env);
         }
     }
 }
