90afb7d89e3cf0f05056cba11305c95b8debc168
Bob Coker <rcoker06@gmail.com>
2019-03-17 17:09:33 -0500

    Database and entities

---
+++ ngStore/Database/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ngStore.Database.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string OrderNumber { get; set; }
+        public int CustomerId { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
