7665b6781db6d75f7598fff88a4da866b3693de5
Bob Coker <rcoker06@gmail.com>
2019-03-26 20:23:53 -0500

    Checkout list update.

--- ngStore/ngStoreClient/app/checkout/checkoutList.component.ts
+++ ngStore/ngStoreClient/app/checkout/checkoutList.component.ts
@@ -1,12 +1,21 @@
-﻿import { Component } from "@angular/core";
+﻿import { Component, OnInit } from "@angular/core";
+import { Order } from '../order/order';
 
 @Component({
     selector: "checkout-list",
     templateUrl: "checkoutList.component.html",
     styleUrls: []
 })
-export class CheckoutList {
+export class CheckoutList implements OnInit {
+
+    public order: Order;
 
     constructor() {
+    }
+
+    ngOnInit() {
+        var o = localStorage.getItem('order');
+        this.order = JSON.parse(o);
+        console.log(this.order);
     }
 }
